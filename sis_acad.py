8f5fac3eea661c6b4946076b76f8a0f49e62aa2c
Ana P. Melo <152793670+An4PDM@users.noreply.github.com>
2024-12-30 15:47:50 -0300

    Update sis_acad.py

--- sis_acad.py
+++ sis_acad.py
@@ -136,3 +136,89 @@ if escolha == 2:
         for x in result:
             print(x)
 
+# Gerenciamento de materias
+if escolha == 3:
+    acao = int(input('''1: Para inserir uma matéria; 
+2: Para deletar matéria; 
+3: Para atualizar dados da matéria;
+4: Para exibir os dados de todos as matérias: '''))
+    
+
+    id = int(input('id: '))
+    n =input('Nome: ')
+    c = input('Carga horária: ')
+
+
+    if acao == 1:
+        query = '''INSERT INTO materia 
+                            VALUES (%s,%s,%s);'''
+        cursor.execute(query,(id,n,c))
+        bd.commit()
+        print('Matéria registrada com sucesso.')
+
+    elif acao == 2:
+        query = '''DELETE FROM materia WHERE id_m = %s;'''
+        cursor.execute(query,(id,))
+        bd.commit()
+        print('Matéria deletada com sucesso.')
+
+    elif acao == 3:
+        query = '''UPDATE materia SET nome = %s,
+                                    carga_horaria = %s
+                                    WHERE id_m = %s;'''
+        cursor.execute(query,(n,c,id))
+        bd.commit()
+        print('Dados atualizados com sucesso.')
+
+    elif acao == 4:
+        query = '''SELECT * FROM materia;'''
+        cursor.execute(query)
+        result = cursor.fetchall()
+        for x in result:
+            print(x)
+
+# Gerenciamento de notas
+if escolha == 4:
+    acao = int(input('''1: Para inserir uma nota; 
+2: Para deletar nota; 
+3: Para atualizar a nota;
+4: Para exibir as notas do aluno: '''))
+    
+    id = input('ID para a nota: ')
+    ra =input('ra do aluno: ')
+    id_materia = input('ID da matéria: ')
+    n = input('Nota: ')
+
+
+    if acao == 1:
+        query = '''INSERT INTO nota 
+                            VALUES (%s,%s,%s,%s);'''
+        cursor.execute(query,(id,ra,id_materia,n))
+        bd.commit()
+        print('Nota registrada com sucesso.')
+
+    elif acao == 2:
+        query = '''DELETE FROM nota WHERE id_n = %s;'''
+        cursor.execute(query,(id,))
+        bd.commit()
+        print('Nota deletada com sucesso.')
+
+    elif acao == 3:
+        query = '''UPDATE nota SET ra = %s,
+                                    id_m = %s,
+                                    nota = %s
+                                    WHERE id_n = %s;'''
+        cursor.execute(query,(ra,id_materia,n,id))
+        bd.commit()
+        print('Dados atualizados com sucesso.')
+
+    elif acao == 4:
+        query = '''SELECT a.nome AS 'Aluno', m.nome AS 'Matéria', n.nota 
+                                    FROM nota n
+                                    INNER JOIN aluno a ON a.ra = n.ra
+                                    INNER JOIN materia m ON m.id_m = n.id_m;'''
+        cursor.execute(query)
+        result = cursor.fetchall()
+        for x in result:
+            print(x)
+
